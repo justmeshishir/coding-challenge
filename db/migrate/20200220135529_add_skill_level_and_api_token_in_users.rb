8275426273c6f94950e3a34d52a04f28b7ccf9c9
Shishir Thapa <shishirthapa1@gmail.com>
2020-02-20 20:01:11 +0545

    add api token as index

--- db/migrate/20200220135529_add_skill_level_and_api_token_in_users.rb
+++ db/migrate/20200220135529_add_skill_level_and_api_token_in_users.rb
@@ -2,5 +2,6 @@ class AddSkillLevelAndApiTokenInUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :skill_level, :integer, default: 0
     add_column :users, :api_token, :string
+    add_index :users, :api_token
   end
 end
